a98d7e00f0679713bd030267cddec8dbc99bdbb6
enriqueperaltajimenez <enriqueperalta1@icloud.com>
2025-02-25 19:02:59 +0100

    added explanations to the code

--- tenaball_finalproject.py
+++ tenaball_finalproject.py
@@ -41,7 +41,7 @@ def tenaball():
 
     lEnrique_used = ["Lionel Messi", "Luis Suarez", "Neymar Jr", "Sergio Busquets", "Gerard Pique", "Javier Mascherano", "Ivan Rakitic", "Jordi Alba", "Andres Iniesta", "Ter Stegen"]
 
-    #code to chose the tenaball that the player will face
+    # Code to randomly choose the tenaball category and its list for the game
 
     random_tenaball, category  = random.choice([
         (barcelona_signings, "most expensive barça signings"),
@@ -56,24 +56,30 @@ def tenaball():
 
     #so that we can track how many the player has guessed
     correct_answer = []
+    
+    #object that defines the amount of lives you 
     lives = 3
 
-    #Scoreboard object so the player can see each time what player he guessed right
+    #Scoreboard object so to substitute the name of the player by the question mark
     reveal_ans = ["?" for _ in random_tenaball]
 
     #starting the game
     print("Welcome to our Tenaball game mode")
     play = input("Do you want to play or exit?")
+    #if player types exit it will. not run
     if play == "exit":
         print("Bye!")
         return
+    
+    #anything else will start the game
     else:
         print(f"find the top 10 {category}")
 
-
+    #will start a loop that will keep going as long as lives are not 0 or the amount of player guessed is less than 10
         while lives > 0 and len(correct_answer) < 10:
             print(f"\nYou have {lives} lives left")
 
+    #will print the tenaball tower which will allows the player to later see what position of the players he guessed
             print("tenaball tower:")
             for i, name in enumerate(reveal_ans, start = 1):
                 print(f"{i}. {name}")
@@ -82,31 +88,43 @@ def tenaball():
 
         #check if guessed player is in the top 10
             if player in random_tenaball and player not in correct_answer:
-                 #will show what position in the top 10 the player is in
+                 #will define what position in the top 10 the player is in
                 position = random_tenaball.index(player) + 1
+                 # will print what position in the top 10 the player is in
                 print(f"You guessed {player} correctly, he is number {position} on the list")
-                 #will then add the correct player to the list
+                 #will then add the correct player to the list by removing the question mark by the player name
                 reveal_ans[position-1] = player
+                 #adds the player in the list of correct answers so that we can see how many right players
+                 #the user has guessed
                 correct_answer.append(player)
                 lives = 3
 
+
+                #if player has already been guessed
             elif player in correct_answer:
                 print("You already guessed that player")
                 #lives == lives
 
+                #if player is not on the list
             else:
                 print(f"{player} is not on the list")
+                #removes a life
                 lives -= 1
 
+        #conditionalities for the code to see if it should stop
+        #stops if the length of players is the top 10 given
         if len(correct_answer) == 10:
             print("\nYou beat the Tenaball")
 
+        #or it will also stop if lives reach zero
         else:
             print("\nThe Tenaball beat you")
             print(f"The correct players were:")
             for position, player in enumerate(random_tenaball, start=1):
                 print(f"{position}. {player}")
 
+
+#if to allow the code to run
 if __name__ == "__main__":
     tenaball()
 
