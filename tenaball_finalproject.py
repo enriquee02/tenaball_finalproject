bb3058ab1526da0b8466cf740c0f7b680842bebd
enriqueperaltajimenez <enriqueperalta1@icloud.com>
2025-02-25 14:06:13 +0100

    Initial commit

---
+++ tenaball_finalproject.py
@@ -0,0 +1,46 @@
+import random
+#random will be used so that python will choose a random top
+#10 list that needs to be completed by the player
+
+print("Welcome to the Tenaball game mode!")
+
+#the following code will define which list's can be chosen
+#by python
+
+def tenaballs():
+    #each category has been chosen one by one
+    #barcelonas most expensive signings
+
+    barcelona_signings = ["Philippe Coutinho", "Ousmane Dembele", "Antoine Griezmann", "Neymar Jr", "Frenkie de Jong", "Luis Suarez", "Zlatan Ibrahimovic"]
+
+    #most appearances with keeper David de Gea
+
+    degea_app = ["Marcus Rashford", "Chris Smalling", "Anthony Martial", "Juan Mata", "Antonio Valencia", "Ashley Young", "Luke Shaw", "Wayne Rooney", "Phil Jones", "Victor Lindelof"]
+
+    #most combined games for Real Madrid and Juventus
+
+    juve_real = ["Cristian Ronaldo", "Zinedine Zidane", "Gonzalo Higuain", "Alvaro Morata", "Sami Khedira", "Danilo", "Fabio Cannavaro", " Angel Di Maria", "Emereson", "Micheal Laudrup"]
+
+    #most spanish national team appearances
+
+    spain_app = ["Sergio Ramos", "Iker Casillas", "Sergio Busquets", "Xavi", "Andres Iniesta", "Andoni Zubizarreta", "David Silva", "Xabi Alonso", "Fernando Torres", "Cesc Fabregas"]
+
+    #highest all time goalscorers
+
+    top_goals = ["Cristiano Ronaldo", "Lionel Messi", "Pele", "Romario", "Ferenc Puskas", "Josef Bican", "Robert Lewandowski", "Jimmy Jones", "Gerd Muller", "Joe Bambrick"]
+
+
+
+
+
+print("Welcome to the Tenaball game mode!")
+play = input(f"do you want to play or exit?")
+
+
+
+
+
+
+
+
+
